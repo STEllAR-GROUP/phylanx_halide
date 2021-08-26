e0f51a8da960dc38fbcee8c22dc07fb96e989209
Hartmut Kaiser <hartmut.kaiser@gmail.com>
2021-08-26 16:50:41 -0500

    Initial commit of no-op plugin

---
+++ plugin/halide_plugin.cpp
@@ -0,0 +1,14 @@
+// Copyright (c) 2021 Hartmut Kaiser
+//
+// Distributed under the Boost Software License, Version 1.0. (See accompanying
+// file LICENSE_1_0.txt or copy at http://www.boost.org/LICENSE_1_0.txt)
+
+#include <phylanx/config.hpp>
+#include <phylanx/plugins/plugin_factory.hpp>
+
+#include "halide_plugin.hpp"
+
+PHYLANX_REGISTER_PLUGIN_MODULE();
+
+PHYLANX_REGISTER_PLUGIN_FACTORY(harries_plugin,
+    phylanx_halide_plugin::harries::match_data);
---
+++ plugin/harries.hpp
@@ -0,0 +1,59 @@
+// Copyright (c) 2021 Hartmut Kaiser
+//
+// Distributed under the Boost Software License, Version 1.0. (See accompanying
+// file LICENSE_1_0.txt or copy at http://www.boost.org/LICENSE_1_0.txt)
+
+#pragma once
+
+#include <phylanx/config.hpp>
+#include <phylanx/execution_tree/primitives/base_primitive.hpp>
+#include <phylanx/execution_tree/primitives/primitive_component_base.hpp>
+
+#include <hpx/future.hpp>
+
+#include <memory>
+#include <string>
+#include <utility>
+#include <vector>
+
+namespace phylanx_halide_plugin {
+
+    class harries
+      : public phylanx::execution_tree::primitives::primitive_component_base
+      , public std::enable_shared_from_this<harries>
+    {
+    private:
+        using primitive_argument_type =
+            phylanx::execution_tree::primitive_argument_type;
+        using primitive_arguments_type =
+            phylanx::execution_tree::primitive_arguments_type;
+        using eval_context = phylanx::execution_tree::eval_context;
+
+    protected:
+        hpx::future<primitive_argument_type> eval(
+            primitive_arguments_type const& operands,
+            primitive_arguments_type const& args) const;
+
+    public:
+        static phylanx::execution_tree::match_pattern_type const match_data;
+
+        harries() = default;
+
+        harries(primitive_arguments_type&& operands,
+            std::string const& name, std::string const& codename);
+
+        hpx::future<primitive_argument_type> eval(
+            primitive_arguments_type const& params,
+            primitive_arguments_type const& args,
+            eval_context ctx) const override;
+    };
+
+    inline phylanx::execution_tree::primitive create_harries(
+        hpx::id_type const& locality,
+        phylanx::execution_tree::primitive_arguments_type&& operands,
+        std::string const& name = "", std::string const& codename = "")
+    {
+        return phylanx::execution_tree::create_primitive_component(
+            locality, "harries", std::move(operands), name, codename);
+    }
+}
