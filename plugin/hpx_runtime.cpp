0ba52478acc56aeb2519035651af4069f44aa67a
R. Tohid <tohid.rod@gmail.com>
2021-08-28 15:39:17 -0500

    Making sure Halide is included before HPX.
    This avoids issues arise by Macros redefined by Halide.

--- plugin/hpx_runtime.cpp
+++ plugin/hpx_runtime.cpp
@@ -1,10 +1,10 @@
+#include <Halide.h>
+#include <HalideRuntime.h>
+
 #include <hpx/hpx_init.hpp>
 #include <hpx/iostream.hpp>
 
 #include <hpx/include/parallel_for_loop.hpp>
-
-#include <Halide.h>
-#include <HalideRuntime.h>
 
 extern "C" int hpx_halide_do_par_for(void *ctx, int (*f)(void *, int, uint8_t *),
                               int min, int extent, uint8_t *closure) {
